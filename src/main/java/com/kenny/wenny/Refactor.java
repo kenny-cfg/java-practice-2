ee9b6f9770e3146eea36895453b392c01ba8c744
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-10 20:15:02 +0100

    Refactor into a function

--- src/main/java/com/kenny/wenny/Refactor.java
+++ src/main/java/com/kenny/wenny/Refactor.java
@@ -18,19 +18,20 @@ public class Refactor {
         }
         System.out.println("Result: " + result);
          */
-
         int num1 = 10;
         int num2 = 5;
         String operation = "+";
-        int result;
-        if (operation.equals("+")) {
-            result = num1 + num2;
-        } else if (operation.equals("-")) {
-            result = num1 - num2;
-        } else {
-            System.out.println("Invalid operation!");
-            return;
-        }
+        int result = performOperation(num1, num2, operation);
         System.out.println("Result: " + result);
+    }
+
+    public static int performOperation(int first, int second, String operation) {
+        if (operation.equals("+")) {
+            return first + second;
+        } else if (operation.equals("-")) {
+            return first - second;
+        } else {
+            throw new RuntimeException("Invalid operation!");
+        }
     }
 }
