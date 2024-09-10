48f7f5d222c3b27139600078081ab5544d5811db
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-10 20:17:18 +0100

    new switch style statement

--- src/main/java/com/kenny/wenny/Refactor.java
+++ src/main/java/com/kenny/wenny/Refactor.java
@@ -20,19 +20,16 @@ public class Refactor {
          */
         int num1 = 10;
         int num2 = 5;
-        String operation = "*";
+        String operation = "+";
         int result = performOperation(num1, num2, operation);
         System.out.println("Result: " + result);
     }
 
     public static int performOperation(int first, int second, String operation) {
-        switch (operation) {
-            case "+":
-                return first + second;
-            case "-":
-                return first - second;
-            default:
-                throw new RuntimeException("Invalid operation!");
-        }
+        return switch (operation) {
+            case "+" -> first + second;
+            case "-" -> first - second;
+            default -> throw new RuntimeException("Invalid operation!");
+        };
     }
 }
