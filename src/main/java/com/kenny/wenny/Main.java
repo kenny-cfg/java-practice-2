a4321ff223cf295d2027db887ec8e43bf9b7a7a3
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-10 20:12:47 +0100

    New refactor class

--- src/main/java/com/kenny/wenny/Main.java
+++ src/main/java/com/kenny/wenny/Main.java
@@ -16,40 +16,7 @@ public class Main {
 //        exercise3();
 //        exercise4();
 //        exercise4WithTernary();
-        refactorExample();
-    }
-
-    public static void refactorExample() {
-        /*
-        Original code:
-        int num1 = 10;
-        int num2 = 5;
-        String operation = "+";
-        int result;
-        if (operation.equals("+")) {
-            result = num1 + num2;
-        } else if (operation.equals("-")) {
-            result = num1 - num2;
-        } else {
-            System.out.println("Invalid operation!");
-            return;
-        }
-        System.out.println("Result: " + result);
-         */
-
-        int num1 = 10;
-        int num2 = 5;
-        String operation = "+";
-        int result;
-        if (operation.equals("+")) {
-            result = num1 + num2;
-        } else if (operation.equals("-")) {
-            result = num1 - num2;
-        } else {
-            System.out.println("Invalid operation!");
-            return;
-        }
-        System.out.println("Result: " + result);
+        Refactor.refactorExample();
     }
 
     /*
---
+++ src/main/java/com/kenny/wenny/Refactor.java
@@ -0,0 +1,36 @@
+package com.kenny.wenny;
+
+public class Refactor {
+    public static void refactorExample() {
+        /*
+        Original code:
+        int num1 = 10;
+        int num2 = 5;
+        String operation = "+";
+        int result;
+        if (operation.equals("+")) {
+            result = num1 + num2;
+        } else if (operation.equals("-")) {
+            result = num1 - num2;
+        } else {
+            System.out.println("Invalid operation!");
+            return;
+        }
+        System.out.println("Result: " + result);
+         */
+
+        int num1 = 10;
+        int num2 = 5;
+        String operation = "+";
+        int result;
+        if (operation.equals("+")) {
+            result = num1 + num2;
+        } else if (operation.equals("-")) {
+            result = num1 - num2;
+        } else {
+            System.out.println("Invalid operation!");
+            return;
+        }
+        System.out.println("Result: " + result);
+    }
+}
